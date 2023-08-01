c470728263afc5791f1babbb135e08906a0f6932
Nguyen <johnny.nguyen@nordicsemi.no>
2023-08-01 15:52:38 -0700

    removed build artifacts.

--- build/image_preload.cmake
+++
@@ -1,8 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "nrf7002dk_nrf5340_cpuapp_ns" CACHE INTERNAL "NCS child image controlled")
-set(BOARD_ROOT "c:/nrf/throughput;c:/nrf/llpm;c:/nrf/peripheral_lbs;c:/nrf/valve_llpm_experiments/nordic/llpm_experiments_1khz/llpm_experiments_1khz/llpm_central;c:/nrf/valve_llpm_experiments/nordic/llpm_experiments_1khz/llpm_experiments_1khz/llpm_peripheral;c:/nrf/lpuart;C:/ncs/v2.4.0/nrf;C:/ncs/v2.4.0/zephyr" CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE "C:/nrf/ctrl-ap/approtect/prj.conf" CACHE INTERNAL "NCS child image controlled")
-set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "C:\ncs\toolchains\31f4403e35\opt\bin\python.exe" CACHE INTERNAL "NCS child image controlled")
