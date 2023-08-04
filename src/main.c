03d70d074dea18f7c5310c6604427b2ba12f7f81
Nguyen <johnny.nguyen@nordicsemi.no>
2023-08-03 18:10:55 -0700

    commented out redundancy

--- src/main.c
+++ src/main.c
@@ -7,12 +7,12 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 
-static void config_nvmc(uint32_t val)
-{
-    while (!NRF_NVMC_S->READY);
-    NRF_NVMC_S->CONFIG = val;
-    while (!NRF_NVMC_S->READY);
-}
+// static void config_nvmc(uint32_t val)
+// {
+//     while (!NRF_NVMC_S->READY);
+//     NRF_NVMC_S->CONFIG = val;
+//     while (!NRF_NVMC_S->READY);
+// }
 
 void main(void)
 {
@@ -24,14 +24,13 @@ void main(void)
     // Somehow, reading the value back does not work.
     // printk("NRF_CTRLAP_S->ERASEPROTECT.DISABLE: %08x\n",NRF_CTRLAP_S->ERASEPROTECT.DISABLE);
     //NRF_CTRLAP_S->SECUREAPPROTECT.DISABLE = 0x00000001;
-
-    if(!NRF_UICR_S->APPROTECT){
-        config_nvmc(NVMC_CONFIG_WEN_Wen);
-        //NRF_UICR_S->APPROTECT=0x000000FF;
-        NRF_UICR_S->APPROTECT=0x50FA50FA; // https://infocenter.nordicsemi.com/index.jsp?topic=%2Fps_nrf5340%2Fchapters%2Fuicr%2Fdoc%2Fuicr.html&resultof=%22%41%70%70%6c%69%63%61%74%69%6f%6e%22%20%22%61%70%70%6c%69%63%22%20%22%43%6f%72%65%22%20%22%63%6f%72%65%22%20%22%55%49%43%52%22%20%22%75%69%63%72%22%20&anchor=register.APPROTECT
-        config_nvmc(NVMC_CONFIG_WEN_Ren);
-        NVIC_SystemReset();
-    }
+    // if(!NRF_UICR_S->APPROTECT){
+    //     config_nvmc(NVMC_CONFIG_WEN_Wen);
+    //     //NRF_UICR_S->APPROTECT=0x000000FF;
+    //     NRF_UICR_S->APPROTECT=0x50FA50FA; // https://infocenter.nordicsemi.com/index.jsp?topic=%2Fps_nrf5340%2Fchapters%2Fuicr%2Fdoc%2Fuicr.html&resultof=%22%41%70%70%6c%69%63%61%74%69%6f%6e%22%20%22%61%70%70%6c%69%63%22%20%22%43%6f%72%65%22%20%22%63%6f%72%65%22%20%22%55%49%43%52%22%20%22%75%69%63%72%22%20&anchor=register.APPROTECT
+    //     config_nvmc(NVMC_CONFIG_WEN_Ren);
+    //     NVIC_SystemReset();
+    // }
 
     printk("Entering forever loop.\n");
     printk("Disclaimer: This example does not lock netcore, and can be unlocked by recovering netcore!\n See README for more information.\n");
